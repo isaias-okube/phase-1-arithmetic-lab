4a551ceb2e7388a7413a8790a77686944b9a4ea1
ISAIAS <issuyep@yahoo.com>
2023-04-01 23:54:32 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// Write your code here!
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max =(1, 2, 10, 18, 20);
+
+let random = 10
+Math.floor(math.random() * 10) + 1
